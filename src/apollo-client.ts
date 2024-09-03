7490eb76b7d1fa1f1446e2f0ee8955d7a70d9870
vudinhphong <vudinhphong.261001@gmail.com>
2024-09-03 21:18:29 +0700

    Update AppoClient

--- src/apollo-client.ts
+++ src/apollo-client.ts
@@ -1,34 +1,7 @@
-import {
-  ApolloClient,
-  InMemoryCache,
-  HttpLink,
-  ApolloLink,
-} from "@apollo/client";
-
-// Tạo các link HTTP riêng biệt cho từng URI
-const link1 = new HttpLink({
-  uri: import.meta.env.VITE_REACT_APP_GRAPHQL_URI2,
-});
-const link2 = new HttpLink({
-  uri: import.meta.env.VITE_REACT_APP_GRAPHQL_URI3,
-});
-
-// Tạo một ApolloLink để thử kết nối đến link1 trước, nếu thất bại thì chuyển sang link2
-const fallbackLink = ApolloLink.from([
-  new ApolloLink((operation, forward) =>
-    forward(operation).map((response) => {
-      if (!response.errors) {
-        // Nếu không có lỗi thì trả về response từ URI đầu tiên (link1)
-        return response;
-      }
-      throw new Error("Primary backend failed");
-    })
-  ).concat(link1),
-  link2,
-]);
+import { ApolloClient, InMemoryCache } from "@apollo/client";
 
 const client = new ApolloClient({
-  link: fallbackLink,
+  uri: import.meta.env.VITE_REACT_APP_GRAPHQL_URI,
   cache: new InMemoryCache(),
 });
 
